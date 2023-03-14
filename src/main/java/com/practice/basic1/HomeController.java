d7471587ac8c99de6b9f76576d9471d23756b809
chener <49267211+chanosong@users.noreply.github.com>
2023-03-14 13:41:42 +0900

    feat: addPeople

--- src/main/java/com/practice/basic1/HomeController.java
+++ src/main/java/com/practice/basic1/HomeController.java
@@ -1,14 +1,22 @@
 package com.practice.basic1;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HomeController {
 
     int cnt = 0;
+    List<Map<String, Object>> peopleList = new ArrayList<>();
 
     @GetMapping("/home/main")
     @ResponseBody
@@ -38,5 +46,26 @@ public class HomeController {
     @ResponseBody
     public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
         return a + b;
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public String showPeople() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(peopleList);
+    }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPeople(String name, int age) {
+        Map<String, Object> map = new LinkedHashMap<>();
+
+        map.put("id", peopleList.size() + 1);
+        map.put("name", name);
+        map.put("age", age);
+
+        peopleList.add(map);
+
+        return peopleList.size() + "번 사람이 추가되었습니다.";
     }
 }
