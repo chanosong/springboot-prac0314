0cba3fc6b43b99993dd50c5e0760fe2997da544b
chener <49267211+chanosong@users.noreply.github.com>
2023-03-14 12:54:34 +0900

    feat: HomeController

---
+++ src/main/java/com/practice/basic1/HomeController.java
@@ -0,0 +1,35 @@
+package com.practice.basic1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    int cnt = 0;
+
+    @GetMapping("/home/main")
+    @ResponseBody
+    public String showMain() {
+        return "안녕하세요.";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2() {
+        return "반갑습니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3() {
+        return "즐거웠습니다.";
+    }
+
+    @GetMapping("/home/count")
+    @ResponseBody
+    public String showCount() {
+        return String.valueOf(cnt++);
+    }
+}
