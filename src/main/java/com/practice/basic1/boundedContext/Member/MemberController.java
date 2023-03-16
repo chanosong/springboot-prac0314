29b69839da3594462c7822a8847ee26204cabee0
chener <49267211+chanosong@users.noreply.github.com>
2023-03-16 15:05:09 +0900

    refact: Alter method of /member/doLogin to POST

--- src/main/java/com/practice/basic1/boundedContext/Member/MemberController.java
+++ src/main/java/com/practice/basic1/boundedContext/Member/MemberController.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
@@ -28,7 +29,7 @@ public class MemberController {
         return "usr/member/login";
     }
 
-    @GetMapping("/member/doLogin")
+    @PostMapping("/member/doLogin")
     @ResponseBody
     public String doLogin(HttpServletRequest req, HttpServletResponse res) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
